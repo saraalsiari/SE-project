8f30c4ee47be53e3111c9b36a71afc300e4c692e
sumayyahA <42795250+sumayyahA@users.noreply.github.com>
2020-10-04 19:00:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let input = document.querySelector(input[id="file-selector"]);
+let input = document.querySelector('input');
 let textarea = document.querySelector('textarea');
  
 input.addEventListener('change', () => {
